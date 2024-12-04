676560da1d839816c0132f0972714e80ea3e706e
anmol02singh <anmol_singh02@outlook.com>
2024-12-04 02:53:17 -0500

    started mobile nav

--- js/main.js
+++ js/main.js
@@ -1,13 +1,33 @@
-function loadSection(id, file) {
-    fetch(file)
-      .then(response => response.text())
-      .then(data => {
-        document.getElementById(id).innerHTML = data;
+function loadSection(id, file, callback) {
+  fetch(file)
+      .then((response) => response.text())
+      .then((data) => {
+          document.getElementById(id).innerHTML = data;
+          if (callback) callback();
       })
-      .catch(error => console.error('Error loading section:', error));
+      .catch((error) => console.error('Error loading section:', error));
 }
 
-loadSection('main-page', 'html/main-page.html');
+loadSection('main-page', 'html/main-page.html', () => {
+  const menuIcon = document.querySelector('.menu-icon img');
+  const mobileMenuOverlay = document.querySelector('.mobile-menu-overlay');
+  const closeMenuButton = document.querySelector('.close-menu');
+
+  menuIcon.addEventListener('click', () => {
+      mobileMenuOverlay.style.display = 'block';
+  });
+
+  closeMenuButton.addEventListener('click', () => {
+      mobileMenuOverlay.style.display = 'none';
+  });
+
+  mobileMenuOverlay.addEventListener('click', (e) => {
+      if (e.target === mobileMenuOverlay) {
+          mobileMenuOverlay.style.display = 'none';
+      }
+  });
+
+});
 loadSection('featured-jobs', 'html/featured-jobs.html');
 loadSection('resume', 'html/resume.html');
 loadSection('hiring', 'html/hiring.html');
@@ -18,5 +38,3 @@ loadSection('portal', 'html/portal.html');
 loadSection('education', 'html/education.html');
 loadSection('boost', 'html/boost.html');
 loadSection('footer', 'html/footer.html');
-
-//drop down menus make em hovers
